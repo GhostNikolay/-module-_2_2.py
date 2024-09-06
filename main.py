f03c00ecd38be23755738e11d20a68a4dadbfdb7
GhostNikolay <snl-10@mail.ru>
2024-09-06 18:59:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+first =  (123)
+second = (456)
+third = (789)
+if first != second and first != third:
+    print(0)
+first = (123)
+second = (456)
+third = (123)
+if first == second or first == third:
+    print(2)
+first = (123)
+second = (123)
+third = (123)
+if first == second and first == third:
+    print(3)
